6ab88016e68f629bfe5e5427af8bd79b3d740495
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-04-05 23:11:25 -0400

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,36 +8,11 @@ def test_main(mocker: MockerFixture) -> None:
         "sys.argv",
         [
             "geojson2osm",
-            "input.geojson",
+            "tests/files/input.geojson",
             "output.xml"
         ],
     )
-    geojson_data = {
-        "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "properties": {"building": "yes"},
-                "geometry": {
-                    "type": "Polygon",
-                    "coordinates": [
-                        [
-                            [36.77125867456199, 37.2584759589531],
-                            [36.77126068621875, 37.2584759589531],
-                            [36.77126102149488, 37.258475158401765],
-                            [36.771257668733604, 37.25847489155132],
-                            [36.77125867456199, 37.2584759589531],
-                        ]
-                    ],
-                },
-            }
-        ],
-    }
     main()
-    osm_xml = g2o(geojson_data)
-    assert '<osm version="0.6" generator="geojson2osm">' in osm_xml
-    assert '<tag k="building" v="yes" />' in osm_xml
-
-    # Check for the presence of a way element with a closed loop
-    way_pattern = re.compile(r'<way id="-\d+">.*?</way>', re.DOTALL)
-    assert re.search(way_pattern, osm_xml)
+    f = open("output.xml", "r")
+    file = f.read()
+    assert file == '<osm version="0.6" generator="geojson2osm">'
